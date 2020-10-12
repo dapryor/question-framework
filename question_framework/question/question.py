537a7f2bfbe39806e27e7936e0984c4222b3345d
Çağatay <cagatayyucelen@gmail.com>
2020-10-12 17:51:14 +0300

    repeated question behaviour changed, user input refactored

--- question_framework/question/question.py
+++ question_framework/question/question.py
@@ -3,7 +3,8 @@ import logging
 
 logger = logging.getLogger("question_framework")
 
-class Question:
+
+class Question():
     def __init__(self, name, text, validation=lambda x: x is not None, post_process=lambda x: x):
         self.name = name
         self.text = text
@@ -13,11 +14,24 @@ class Question:
     def __hash__(self):
         return hash(self.name)
 
+    def get_answer(self):
+        answer = None
+        while answer is None or not self.verification(answer):
+            answer = input(self.text + "\n")
+        return self.post_process(answer)
+
+    def ask(self):
+        return {self.name: self.get_answer()}
+
 
 class RepeatedQuestion(Question):
-    def __init__(self, name, text, repeated_questions,):
-        super(RepeatedQuestion, self).__init__(name, text, isint, int)
-        self.repeated_questions = repeated_questions
+    def __init__(self, name, text, ask_count, validation=lambda x: x is not None, post_process=lambda x: x):
+        super(RepeatedQuestion, self).__init__(name, text, validation=validation, post_process=post_process)
+        self.ask_count = ask_count
+
+    def ask(self):
+        logger.debug(f"DEBUG: REPEAT Question: {self.name}")
+        return {self.name: list(map(lambda q: q.get_answer(), [self] * self.ask_count))}
 
 
 class BranchedQuestion(Question):
@@ -26,3 +40,11 @@ class BranchedQuestion(Question):
         branched_validation = pick_from_choices(question_dict)
         super(BranchedQuestion, self).__init__(name, text, branched_validation)
         self.question_branches = question_dict
+
+    def ask(self):
+        logger.debug(f"DEBUG: BRANCH Question: {self.name}")
+        ret = dict()
+        branch_answer = self.get_answer()
+        ret[self.name] = self.question_branches[branch_answer].ask()
+        ret[self.name]["__answer"] = branch_answer
+        return ret
--- question_framework/user_input/user_input.py
+++ question_framework/user_input/user_input.py
@@ -1,39 +1,11 @@
 import os
 import logging
-
 from question_framework.question import Question, RepeatedQuestion, BranchedQuestion
-import logging
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 logger = logging.getLogger("question_framework")
 
-def ask_for_input(questions: list):
-    def get_input_loop(question):
-        tmp = None
-        while tmp is None or not question.verification(tmp):
-            tmp = input(question.text + "\n")
-        return question.post_process(tmp)
 
-    def loop_through_question(questions):
-        ret = dict()
-        if isinstance(questions, Question):
-            questions = [questions]
-        for question in questions:
-            if isinstance(question, RepeatedQuestion):
-                logger.debug(f"DEBUG: REPEAT Question: {question.name}")
-                loop_cnt = get_input_loop(question)
-                ret[question.name] = []
-                for i in range(loop_cnt):
-                    looped_answers = loop_through_question(question.repeated_questions)
-                    ret[question.name].append(looped_answers)
-            elif isinstance(question, BranchedQuestion):
-                logger.debug(f"DEBUG: BRANCH Question: {question.name}")
-                branch_answer = get_input_loop(question)
-                ret[question.name] = loop_through_question(question.question_branches[branch_answer])
-                ret[question.name]["__answer"] = branch_answer
-            else:
-                logger.debug(f"DEBUG: NORMAL Question: {question.name}")
-                ret[question.name] = get_input_loop(question)
-        return ret
-
-    return loop_through_question(questions)
+def ask(questions: list):
+    answerDicts = list(map(lambda q: q.ask(), questions))
+    return {k: v for a in answerDicts for k, v in a.items()}
