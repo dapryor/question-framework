0e77c52bd6894ff741d7721e08f122f0b9191457
Çağatay <cagatayyucelen@gmail.com>
2020-10-12 20:25:51 +0300

    unused imports and lines deleted, map unwrapped from list

--- question_framework/user_input/user_input.py
+++ question_framework/user_input/user_input.py
@@ -1,11 +1,9 @@
 import os
-import logging
 from question_framework.question import Question, RepeatedQuestion, BranchedQuestion
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-logger = logging.getLogger("question_framework")
 
 
 def ask(questions: list):
-    answerDicts = list(map(lambda q: q.ask(), questions))
+    answerDicts = map(lambda q: q.ask(), questions)
     return {k: v for a in answerDicts for k, v in a.items()}
