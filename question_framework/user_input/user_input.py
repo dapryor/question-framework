b82bfe13a4a94ef4ac441f1d83450bd743bd05fb
David Pryor <dapryor@ncsu.edu>
2020-10-12 23:15:01 -0400

    Update user_input.py

--- question_framework/user_input/user_input.py
+++ question_framework/user_input/user_input.py
@@ -1,9 +1,3 @@
-import os
-from question_framework.question import Question, RepeatedQuestion, BranchedQuestion
-
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-
 def ask(questions: list):
     answerDicts = map(lambda q: q.ask(), questions)
     return {k: v for a in answerDicts for k, v in a.items()}
