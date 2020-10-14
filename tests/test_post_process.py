00543676be648b3e81470eaf8cd36c283f8fef09
zebralt <15049142+Zebralt@users.noreply.github.com>
2020-10-14 23:50:38 +0200

    Update test_post_process.py

--- tests/test_post_process.py
+++ tests/test_post_process.py
@@ -1,2 +1,13 @@
-def test_as_int_range():
-  
+
+
+def test_as_list():
+    assert as_list("3 4") == ["3", "4"]
+    assert as_list("3 4 ") == ["3", "4"]
+    assert as_list("3  4 ") == ["3", "4"]
+    assert as_list("3, 4", sep=',') == ["3", "4"]
+
+    
+def test_as_list_of():
+    assert as_list_of(int)("3 4") == [3, 4]
+    assert as_list_of(float)("3.5; 6", sep=";") == [3.5, 6.0]
+    
