ca21ddb980d3d8c31c8524fc8691acc1a4f1608a
zebralt <15049142+Zebralt@users.noreply.github.com>
2020-10-14 23:44:09 +0200

    Make better regexes for as_int_range (+12 squashed commit)
    Squashed commit:
    
    [472522a] Fix typo
    
    [58f49ea] Fix typo
    
    [af16de9] Add test for mapped_to mismatched lens error
    
    [6971db0] Reorganize post_process.py
    
    [55e0d06] Reorganize post_process.py
    
    [f13aab5] Remove extra whitespace + Add mapped post processor
    
    [eb4a9fe] Remove unneeded import
    
    [0333021] Add various post processors
    
    [e0b7e6e] Update post_process.py
    
    [ef44411] Update post_process.py
    
    [0054367] Update test_post_process.py
    
    [b8f0e60] Create test_post_process.py

--- question_framework/post_process/post_process.py
+++ question_framework/post_process/post_process.py
@@ -1,4 +1,7 @@
 from ipaddress import ip_address
+from typing import Callable, List, Optional, Any
+from functools import partial
+import re
 
 
 def ip_range_to_list(x):
@@ -12,3 +15,71 @@ def ip_range_to_list(x):
     ip1, ip2 = x.replace(" ", "").split("-")
 
     return ip_range_generator(ip1, ip2)
+
+
+def as_list(ans: str, sep: str = ',', f: Optional[Callable[[str], Any]] = None) -> List[Any]:
+    """
+    Parse list elements and optionally apply a transformer to each element.
+    This allows you to parse things directly as the type they should be, e.g. integers:
+    as_list("3, 4", f=int) == [3, 4]
+    """
+    items = ans.strip().split(sep)
+    items = map(str.strip, items)
+    if f is not None:
+        items = map(f, items)
+    return list(items)
+
+
+def as_list_of(f: Callable) -> Callable[[str], List[Any]]:
+    """
+    A helper function to build as_list alternatives on the spot.
+    as_ints = as_list_of(int)
+    as_ints("3, 4") == [3, 4]
+    """
+    return partial(as_list, f=f)
+
+
+as_ints = as_list_of(int)
+as_floats = as_list_of(float)
+
+
+def mapped_to(*fs: Callable) -> Callable[[str], List[Any]]:
+    """
+    Apply a given function to the item found at a given index.
+    as_mapping(int, float)("3, 3.5") == [3, 3.5]
+    Both lists need to have the same length.
+    """
+    def mapper(ans: str, **kw):
+        items = as_list(ans)
+        if len(items) != len(fs):
+            raise ValueError('Key & Input lists have mismatched lengths')
+        items = (f(i) for i, f in zip(items, fs))
+        return list(items)
+    return mapper
+
+
+def as_int_range(ans: str) -> range:
+    """
+    Compute an integer range from a string representation. Several options:
+    dash notation: 3 - 5 (only positive values)
+    python slice notation: :3 (start implied to be 0), 3:4 (normal), 3:55:2 (with step)
+    natural language: 'from 3 to 5' or '-55 to 55'
+    """
+    ans = ans.strip()
+    if re.match(r'^\s*\d+\s*-\s*\d+$', ans):
+        start, end = ans.split('-')
+        start = start.strip()
+        end = end.strip()
+        return range(int(start), int(end))
+    if re.match(r'^(-?\d+)?:-?\d+(:-?\d+)?$', ans):
+        start, end, *step = ans.split(':')
+        start = int(start or 0)
+        step = int(step and step[0] or 1)
+        return range(start, int(end), step)
+    if re.match(r'^(from\s)?\s*-?\d+\s+to\s+-?\d+$', ans, flags=re.IGNORECASE):
+        *_, start, _, end = ans.split()
+        start = int(start)
+        end = int(end)
+        step = 1 if end >= start else -1
+        return range(start, end, step)
+    raise ValueError(f'Could not transform {ans!r} into an integer range')
---
+++ tests/test_post_process.py
@@ -0,0 +1,58 @@
+from question_framework.post_process import *
+
+
+def test_as_list():
+    assert as_list("3, 4") == ["3", "4"]
+    assert as_list("3,4") == ["3", "4"]
+    assert as_list("3 , 4 ") == ["3", "4"]
+
+
+class Value:
+    def __init__(self, value):
+        self.value = value
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.value == other.value
+
+
+def test_as_list_of():
+
+    assert as_list_of(int)("3, 4") == [3, 4]
+    assert as_list_of(float)("3.5; 6", sep=";") == [3.5, 6.0]
+    assert as_list_of(Value)("33, allo") == [Value("33"), Value("allo")]
+
+
+def test_mapped_to():
+
+    assert mapped_to(int, float, str)("34, 34.5, jon") == [34, 34.5, "jon"]
+
+    try:
+        mapped_to(int, float)("34, 34, 34")
+    except ValueError as ve:
+        assert str(ve).endswith('mismatched lengths')
+
+    class IntValue(Value):
+        def __init__(self, value): super().__init__(int(value))
+    class FloatValue(Value):
+        def __init__(self, value): super().__init__(float(value))
+
+    assert mapped_to(IntValue, FloatValue, str)("34, 34.5, jon") == [
+        IntValue(34),
+        FloatValue(34.5),
+        "jon"
+    ]
+
+def test_as_int_range():
+
+    assert as_int_range("3-66") == range(3, 66)
+    assert as_int_range("0 - 0") == range(0)
+
+    assert as_int_range(":33:3") == range(0, 33, 3)
+    assert as_int_range(":33") == range(0, 33)
+    assert as_int_range(":33") == range(0, 33)
+    assert as_int_range("0:33") == range(0, 33)
+    assert as_int_range("-28:33") == range(-28, 33)
+
+    assert as_int_range("from 333 to -28") == range(333, -28, -1)
+    assert as_int_range("from -333 to -28") == range(-333, -28, 1)
+    assert as_int_range("from -333 to 28") == range(-333, 28, 1)
+    assert as_int_range("333 to 2833") == range(333, 2833, 1)
