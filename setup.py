1a77d52dcd1a7cd3808cd853c9d99c3641e2e1ce
dapryor <dapryor@ncsu.edu>
2020-10-19 20:49:51 -0400

    Updated to 0.1.3 - LICENSE is now MIT

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+import pathlib
 import setuptools
 
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setuptools.setup(
     name="question_framework", # Replace with your own username
-    version="0.0.1",
+    version="0.1.3",
     author="David Pryor",
     author_email="dapryor@cisco.com",
     description="Framework for asking questions",
-    url="https://github.com/dapryor/question_framework",
-    packages=setuptools.find_packages(),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dapryor/question-framework",
+    license="MIT",
+    packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
