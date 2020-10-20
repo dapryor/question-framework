3f260f89af9f289c318249c85cacbab1db338068
Ricky Tharrington <ricky.tharrington@gmail.com>
2020-10-19 22:14:53 -0400

    feat: Static Answers

--- tests/test_ask.py
+++ tests/test_ask.py
@@ -1,6 +1,18 @@
 import mock
+import pytest
 from question_framework.question import *
 from question_framework.user_input import *
+
+
+@pytest.fixture
+def static_test_questions():
+    return [
+        BranchedQuestion("password?", "Do you want to enter a password? [y|n]", [
+            Question("y", "What is your password?"),
+            StaticAnswer("n", "They said no.")
+        ]
+        )
+    ]
 
 
 class TestAsk():
@@ -21,3 +33,27 @@ class TestAsk():
             questions = [RepeatedQuestion("Password", "Your password:", 3)]
             answers = ask(questions)
             assert(answers == {'Password': ['321', '123', '765']})
+
+    def test_static_answer_1(self, static_test_questions):
+
+        def answer_bank(key):
+            return {
+                "Do you want to enter a password? [y|n]\n": "y",
+                "What is your password?\n": "123"
+            }[key]
+
+        with mock.patch("builtins.input", answer_bank):
+            answers = ask(static_test_questions)
+            assert(answers == {"password?": {"__answer": "y", "y": "123"}})
+
+    def test_static_answer_2(self, static_test_questions):
+
+        def answer_bank(key):
+            return {
+                "Do you want to enter a password? [y|n]\n": "n"
+            }[key]
+
+        with mock.patch("builtins.input", answer_bank):
+            answers = ask(static_test_questions)
+            assert(answers == {"password?": {
+                   "__answer": "n", "n": "They said no."}})
