2b06ee46a90c7094482823a225ee2341729b6ad2
dapryor <dapryor@ncsu.edu>
2021-02-17 00:28:06 -0500

    Added validation utils to chain validations.  all_validation is true when all validations pass. any_validation is true when at least one validation passes.

---
+++ question_framework/validation/utils.py
@@ -0,0 +1,12 @@
+def all_validation(*fn):
+    def chained_fn(x: str):
+        res = (f(x) for f in fn)
+        return all(res)
+    return chained_fn
+
+
+def any_validation(*fn):
+    def chained_fn(x: str):
+        res = (f(x) for f in fn)
+        return any(res)
+    return chained_fn
