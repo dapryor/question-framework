a0353309e0468a4a6281e21c58cf2277f8a7129e
dapryor <dapryor@cisco.com>
2022-10-30 15:40:27 -0400

    Added typing to validation utils. Added typing to user input retrieval util

--- question_framework/validation/utils.py
+++ question_framework/validation/utils.py
@@ -1,13 +1,16 @@
-def all_validation(*fn):
-    def chained_fn(x: str):
+from typing import Callable, List
+
+
+def all_validation(*fn: Callable[[str], str]) -> Callable[[str], bool]:
+    def chained_fn(x: str) -> bool:
         res = (f(x) for f in fn)
         return all(res)
 
     return chained_fn
 
 
-def any_validation(*fn):
-    def chained_fn(x: str):
+def any_validation(*fn: Callable[[str], str]) -> Callable[[str], bool]:
+    def chained_fn(x: str) -> bool:
         res = (f(x) for f in fn)
         return any(res)
 
